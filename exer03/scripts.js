28edce4306526453c50ac760339a169bb30cffdc
Wes <wesleydeveloper97@gmail.com>
2017-04-16 22:52:22 -0300

    Objeto Number

--- exer03/scripts.js
+++ exer03/scripts.js
@@ -11,3 +11,9 @@ console.log(frase.indexOf("dizer"));
 
 //Retorna a partir da posção 0 a posição passada
 console.log(frase.charAt(7));
+
+//Extrai uma quantidade de caracteres a partir da posição inicial
+console.log(frase.substr(2, 4));
+
+//Substitui o conteúdo da String
+console.log(frase.replace("não", "sim"));
---
+++ exer04/scripts.js
@@ -0,0 +1,8 @@
+//NUMBER
+
+
+var number = 20;
+var objNumber = new Number(20);
+
+console.log(number);
+console.log(objNumber);
