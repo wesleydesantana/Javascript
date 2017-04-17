8908450b99323461e83b5c86dc05e1041d5c9b79
Wes <wesleydeveloper97@gmail.com>
2017-04-16 22:14:23 -0300

    Atributos e Métodos

---
+++ exer03/scripts.js
@@ -0,0 +1,13 @@
+// ATRIBUTOS E MÉTODOS
+
+
+var empresa  = "Google";
+var frase = "Foco é dizer não";
+console.log(empresa);
+console.log(empresa.length);  //Tamanho da String
+
+//Posição da parte procurada a partir da posição 0
+console.log(frase.indexOf("dizer"));
+
+//Retorna a partir da posção 0 a posição passada
+console.log(frase.charAt(7));
