14b52e90b1428771769f69fe317c1b5e315bcc22
Wes <wesleydeveloper97@gmail.com>
2017-04-16 23:46:24 -0300

    Operadores

---
+++ exer06/scripts.js
@@ -0,0 +1,41 @@
+// Operadores
+
+var idade = 10;
+var idade2 = 30;
+
+//soma
+console.log(idade + idade2);
+
+//subtração
+console.log(idade - idade2);
+
+//divisão
+console.log(idade2 / idade);
+
+//resto da divisão
+console.log(idade % 2);
+
+//multiplicação
+console.log(idade * idade2);
+
+
+//operadores de atribuição 
+
+console.log(idade+=10);
+
+console.log(idade-=5);
+
+console.log(idade*=10);
+
+console.log(idade/=10);
+
+//Concatenação
+var A = "Wesley";
+var B = "Santana";
+
+console.log(A+B);
+
+var number1 = 1;
+var number2 = 2;
+var number3 = "Valores";
+console.log(number1 +number2 +number3);
