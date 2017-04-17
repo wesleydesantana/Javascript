a5ddb547775cdd861cc9b79a7283e2e3e6c9bf18
Wes <wesleydeveloper97@gmail.com>
2017-04-16 22:00:53 -0300

    Troca de Código

--- exer01/scripts.js
+++ exer01/scripts.js
@@ -1,15 +1,1 @@
-var mensagem = "Olá, Mundo!";
-alert(mensagem);
-mensagem = 10;
-alert(mensagem);
-console.log(mensagem);
-
-var empresa = "Facebook";//literal primitiva
-console.log("Minha empresa: " + empresa);
-console.log("Do tipo: " + typeof empresa);
-console.log("Construtor" + empresa.constructor);
-//-----------------------------------------//
-var curso = new String('Git');
-console.log("Curso :" + curso);
-console.log("Do tipo :" + typeof curso);
-console.log("Construtor :" + curso.constructor);
+alert('Olá, Mundo!');
