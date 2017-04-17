532337e9c4bebe5c225aaf7e828e8d2ffa3c3111
Wes <wesleydeveloper97@gmail.com>
2017-04-16 21:58:04 -0300

    Exercícios 1 e 2

---
+++ exer01/scripts.js
@@ -0,0 +1,15 @@
+var mensagem = "Olá, Mundo!";
+alert(mensagem);
+mensagem = 10;
+alert(mensagem);
+console.log(mensagem);
+
+var empresa = "Facebook";//literal primitiva
+console.log("Minha empresa: " + empresa);
+console.log("Do tipo: " + typeof empresa);
+console.log("Construtor" + empresa.constructor);
+//-----------------------------------------//
+var curso = new String('Git');
+console.log("Curso :" + curso);
+console.log("Do tipo :" + typeof curso);
+console.log("Construtor :" + curso.constructor);
