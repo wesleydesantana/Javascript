9894eeeb1b9bda5bedd28ecee46019b12bc4348b
Wes <wesleydeveloper97@gmail.com>
2017-04-16 23:10:04 -0300

    Number Conversões

--- exer04/scripts.js
+++ exer04/scripts.js
@@ -6,3 +6,21 @@ var objNumber = new Number(20);
 
 console.log(number);
 console.log(objNumber);
+
+//Number - Conversões
+var textoInteiro = "20";
+console.log(typeof textoInteiro);
+
+var inteiro = parseInt(textoInteiro, 10);
+console.log(typeof inteiro);
+
+var textoFloat = "10.22";
+console.log(typeof textoFloat);
+
+var floats = parseFloat(textoFloat);
+console.log(typeof floats);
+
+//toFixed() Está  função retorna uma string
+var mil = 1000;
+var milStr = mil.toFixed(2);
+console.log(typeof milStr);
