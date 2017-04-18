df6fb3536da413fc6747fa20180417996e306a77
Wes <wesleydeveloper97@gmail.com>
2017-04-18 13:15:32 -0300

    Switch

---
+++ switch/scripts.js
@@ -0,0 +1,11 @@
+
+var cidade = "SP";
+switch(cidade){
+	case "SP":
+	alert("Floresta de concreto");
+	break;
+	case "RJ":
+	alert("Cartão postal");
+	break;
+	default: alert("Cidade sem descrição");
+}
