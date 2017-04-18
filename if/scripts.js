7cfc6735191ccaedd918ddbc092622395accd714
Wes <wesleydeveloper97@gmail.com>
2017-04-18 11:29:26 -0300

    if, else if

---
+++ if/scripts.js
@@ -0,0 +1,25 @@
+var num = 20;
+
+
+if(num >= 10){
+	alert('Valor maior ou igual a 10');
+}else{
+	alert('Valor menor que 10');
+}
+
+
+//else if
+
+if(num < 10){
+	alert('Valor menor que 10');
+}else if(num <= 10){
+	alert('Valor menor igual a 10');
+}else{
+	alert('Valor maior que 10')
+}
+
+//if ternário 
+
+var valor = 1;
+var resultado = valor > 2 ? "sim" : "não";
+alert(resultado);
