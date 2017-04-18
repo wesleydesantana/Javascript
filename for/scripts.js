9a801ab86318d47fb874972a7da4899d25e7eba5
Wes <wesleydeveloper97@gmail.com>
2017-04-18 14:10:15 -0300

    For e While

---
+++ for/scripts.js
@@ -0,0 +1,5 @@
+//laço for
+var i = 1;
+for(i;i<=10; i++){
+	console.log("O valor é: " + i);
+}
---
+++ while/scripts.js
@@ -0,0 +1,8 @@
+var i = 0;
+
+while(i <=10){
+	console.log("O contador é: " + i);
+	i++;
+}
+
+console.log("O valor do contador" + i);
