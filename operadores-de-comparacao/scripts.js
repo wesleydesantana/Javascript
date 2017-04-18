387cac00cc7d7e973d3d3d9303953ad9656283c0
Wes <wesleydeveloper97@gmail.com>
2017-04-18 10:26:53 -0300

    Operadores de Comparacao

---
+++ operadores-de-comparacao/scripts.js
@@ -0,0 +1,37 @@
+//Igualdade
+
+var A = 3;
+var B = 6;
+
+//Mesmo Valor
+console.log(A == 3);
+console.log(B == 3);
+
+//Diferente
+console.log(A != 3);
+console.log(A != B);
+
+
+//Identico
+console.log(A === 3);
+console.log(A === "3");
+
+//Retorna true se o tipo e o valor forem diferentes
+
+console.log(A !== 3);
+console.log(A !== "3");
+
+//Maior quê 
+
+console.log(A>B);
+console.log(A<B);
+
+//Maior igual quê 
+
+console.log(A>=3);
+console.log(A>=7);
+
+//Menor igual quê
+
+console.log(A<=3);
+console.log(A<=7);
