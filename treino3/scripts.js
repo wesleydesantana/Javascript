f88b8227be3145456ec5f39698c9aaf4d0938040
Wes <wesleydeveloper97@gmail.com>
2017-04-22 20:34:03 -0300

    Calculando media

---
+++ treino3/scripts.js
@@ -0,0 +1,8 @@
+var valor1 = new Number(prompt('Digite um valor'));
+var valor2 = new Number(prompt('Digite um valor'));
+var valor3 = new Number(prompt('Digite um valor'));
+var valor3 = new Number(prompt('Digite um valor'));
+
+media = (valor1 + valor2 + valor3 + valor3) / 4;
+console.log(typeof media)
+alert(media);
