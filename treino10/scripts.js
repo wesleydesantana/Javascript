29104bc72cb0375f5e56db4d4f196fbc0dc8af48
Wes <wesleydeveloper97@gmail.com>
2017-04-22 22:30:24 -0300

    Treino 10

---
+++ treino10/scripts.js
@@ -0,0 +1,8 @@
+nome 		= new String(prompt('Digite o nome do vendedor'));
+calca 		= new Number(prompt('Digite o valor da calça'));
+quantidade 	= new Number(prompt('Digite a quantidade de calças'));
+
+comissao = 25.00;
+salario = 1000.00;
+salario_total = (quantidade * comissao) + salario;
+alert('Salario Bruto: ' + salario_total)
---
+++ treino9/scripts.js
@@ -0,0 +1,4 @@
+celsius = new  Number(prompt('Digite o valor de Celsius'));
+
+fahrenheit = (9 / 5 * celsius) + 32;
+alert('fahrenheit:' + fahrenheit);
