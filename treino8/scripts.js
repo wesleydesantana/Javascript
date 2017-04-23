d036dbfc6fb97b8af912e9ffda6f781d25d29abb
Wes <wesleydeveloper97@gmail.com>
2017-04-22 21:46:10 -0300

    Calculos

---
+++ treino8/scripts.js
@@ -0,0 +1,12 @@
+valor = new Number(prompt('Digite um valor'));
+
+sucessor = valor + 1;
+antecessor = valor - 1;
+dobro = valor * 2;
+metade = valor / 2;
+
+alert("Valor: " + valor);
+alert("Dobro: " + dobro);
+alert("Antecessor: " + antecessor);
+alert("Sucessor: " + sucessor);
+alert("Metade: " + metade);
