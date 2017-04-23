5628ef3372057f7292c9060750d70124fc688c87
Wes <wesleydeveloper97@gmail.com>
2017-04-22 21:00:23 -0300

    Treino 5

---
+++ treino4/scripts.js
@@ -0,0 +1,5 @@
+var valor1 = 5;
+var valor2 = 10;
+
+alert(valor2 == 5);
+alert(valor1 !=10);
---
+++ treino5/scripts.js
@@ -0,0 +1,7 @@
+valor = new Number(prompt('Digite um valor'));
+
+if(valor > 100){
+	alert('Valor maior que 100');
+}else{
+	alert('Valor menor que 100');
+}
